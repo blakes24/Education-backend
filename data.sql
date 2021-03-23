dffbe38e959de06f5f4fc4bfd496349def791551
blakes24 <blakes24@gmail.com>
2021-03-23 16:36:30 -0700

    Add users and schools tables

--- data.sql
+++ data.sql
@@ -1,28 +1,27 @@
 \c education 
-
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-    username text PRIMARY KEY,
-    password text NOT NULL,
-    first_name text NOT NULL,
-    last_name text NOT NULL,
-    email text NOT NULL,
-    admin BOOLEAN NOT NULL DEFAULT FALSE
-    
+DROP TABLE IF EXISTS schools;
+
+CREATE TABLE schools (
+    id integer PRIMARY KEY,
+    name text,
+    district text,
+    state text
 );
 
-INSERT INTO users (username, password, first_name, last_name, email, admin)
-VALUES ('testuser',
-        'tester1pw',
-        'Test',
-        'User',
-        'testuser@email.com',
-        FALSE),
-       ('testadmin',
-        'adminpw',
-        'Test',
-        'Admin!',
-        'admin@email.com',
-        TRUE);
+CREATE TABLE users (
+    email text PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    school_id integer NOT NULL REFERENCES schools ON DELETE CASCADE,
+    admin boolean NOT NULL DEFAULT FALSE
+);
+
+INSERT INTO schools (id, name, district, state)
+    VALUES (1, 'Cielo Azul', 'Rio Rancho', 'New Mexico');
+
+INSERT INTO users (email, PASSWORD, first_name, last_name, school_id, admin)
+    VALUES ('teacher1@school.edu', 'teacher1pw', 'Carl', 'Smith', 1, FALSE), ('teacher2@school.edu', 'teacher2pw', 'Kate', 'Wilkinson', 1, FALSE), ('teacher3@school.edu', 'teacher3pw', 'Julia', 'Packer', 1, FALSE), ('coach@school.edu', 'coachpw', 'Austin', 'Larkman', 1, TRUE);
 
